37e0a1c5937ad56e23ecd10b76e23a9e7a19e891
Julian Zea <cesarjulianzeabravo@gmail.com>
2022-06-07 15:10:02 -0700

    task 2new

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - Indicates  random number is positive or negative
+ * main - Indicates if a random number is positive or negative
  *
  * Description: Generates a random number.
  *   indicates if it's positive or negative
  *
  * Return: 0
  */
-int main(void) 
-{
-  char alphabet='a';
+
+int main(void) {
+  char alphabet;
+  alphabet = 'a';
+
+  
   while(alphabet<='z')
     {
       putchar(alphabet);
