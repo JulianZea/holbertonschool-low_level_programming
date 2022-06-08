a68330f5c7528bad69692721a438c396d5eb7217
Julian Zea <cesarjulianzeabravo@gmail.com>
2022-06-07 17:52:39 -0700

    task 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - print lowercase alpha a-z but 'q' and 'e'
+ *Return: Always 0 (Success)
+ */
+int main(void)
+	{
+		char alphabet = 'a';
+		while (alphabet <= 'z')
+		{
+			if (alphabet != 'q' && alphabet != 'e')
+			putchar(alphabet);
+			alphabet++;
+		}
+		putchar('\n');
+		return (0)
+	}
+
