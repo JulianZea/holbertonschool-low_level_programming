5ceb7fb53d0d3e1e7a56d12f9f9f7476c7212b64
Julian Zea <cesarjulianzeabravo@gmail.com>
2022-06-07 18:41:47 -0700

    task 4new

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -4,14 +4,14 @@
  *Return: Always 0 (Success)
  */
 int main(void)
+{
+	char alphabet = 'a';
+	while (alphabet <= 'z')
 	{
-		char alphabet = 'a';
-		while (alphabet <= 'z')
-		{
-			if (alphabet != 'q' && alphabet != 'e')
-			putchar(alphabet);
-			alphabet++;
-		}
+		if ((alphabet != 'q') && (alphabet != 'e'))
+		putchar(alphabet);
+		alphabet++;
+	}
 		putchar('\n');
 		return (0);
-	}
+}
