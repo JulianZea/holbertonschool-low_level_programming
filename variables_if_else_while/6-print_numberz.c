e243f13290a1527351eec5938cfe4758bfd244f4
Julian Zea <cesarjulianzeabravo@gmail.com>
2022-06-07 21:34:24 -0700

    task 6

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -2,23 +2,20 @@
 
 /**
  * main - Prints all avaliable numbers in base 10
- *
  * Description: Prints all single digit numbers of base 10 starting from 0
  * followed by a new line
- *
- * Return: 0 Always
+ * Return: Always return 0
  */
 
 int main(void)
 {
-        int i = 48;
+	int i = 0;
 
-        while (i <= 57)
-        {
-                putchar(i);
-                i++;
-        }
-
-        putchar(10);
-        return (0);
+	while (i <= 9)
+	{
+		putchar(i + '0');
+		i++;
+	}
+	putchar(10);
+	return (0);
 }
