8fadfb73da56c81a9f8abf0ec5ff50840860dbe2
Julian Zea <cesarjulianzeabravo@gmail.com>
2022-06-10 13:21:56 -0700

    task 4-3new

--- debugging/main.h
+++ debugging/main.h
@@ -12,11 +12,11 @@ int largest_number(int a, int b, int c);
 /*task 3*/
 
 int convert_day(int month, int day);
-
-void print_remaining_days(int month, int day, int year
 		
 /*task 3*/
 		
 int convert_day(int month, int day);
-void print_remaining_days(int month, int day, int year));
+
+void print_remaining_days(int month, int day, int year);
+
 #endif /* MAIN_H */
