18cb2d22b227d50e628dc8af7af261c8574e6584
Julian Zea <cesarjulianzeabravo@gmail.com>
2022-06-09 21:11:48 -0700

    task 3-5

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -34,4 +34,13 @@ int _islower(int c);
  */
 int _isalpha(int c);
 
+/**
+ * print_sign - 
+ * @e: character to verify
+ * Returns 1 and prints + if n is greater than zero
+ * Returns 0 and prints 0 if n is zero
+ * Returns -1 and prints - if n is less than zero
+ */
+
+int print_sign(int n);
 #endif
