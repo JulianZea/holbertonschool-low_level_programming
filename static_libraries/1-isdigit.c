55ddad9f492563c5588259193274140ce4df3a86
Julian Zea <cesarjulianzeabravo@gmail.com>
2022-06-21 22:39:55 -0700

    subiendo task 2-0

---
+++ static_libraries/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isdigit - code
+ * Description : Evaluate if it is higher or lower
+ *@c:The character
+ * Return: Returns: 1 if greater, otherwise 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 &&  c <= 57)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
