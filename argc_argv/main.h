35a1dfce9e60f6b5b5f68d082cbdeaeb5829c7c3
root <root@DESKTOP-L44MSJ2.localdomain>
2022-06-24 22:35:53 -0500

    task 3-4

--- argc_argv/main.h
+++ argc_argv/main.h
@@ -1,5 +1,15 @@
-#ifndef ULTRON_H
-#define ULTRON_H
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+int main(int argc __attribute__((unused)), char *argv[]);
+
+int main(int argc, char *argv[] __attribute__((unused)));
 
 int main(int argc, char *argv[]);
 
