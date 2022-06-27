a8545beb58ff72c0ec3db7b1ee88752a7a4ba077
root <root@DESKTOP-L44MSJ2.localdomain>
2022-06-27 16:42:30 -0500

    subiendo cambios

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -1,3 +1,9 @@
+#ifndef JARVIS_H
+#define JARVIS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
 /*proyecto 1*/
 /*task 0*/
 void *malloc_checked(unsigned int b);
@@ -7,3 +13,5 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 /*task 3*/
 int *array_range(int min, int max);
+
+#endif /* JARVIS_H */
