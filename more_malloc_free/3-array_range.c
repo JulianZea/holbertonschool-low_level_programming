3fe6252bd0de2e1d8744d06a4970e56c97e939b3
root <root@DESKTOP-L44MSJ2.localdomain>
2022-06-28 13:20:59 -0500

    task 1-3

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * array_range - Creates an array of integers.
+ *
+ * @min: min number to add in the array
+ *
+ * @max: max number to add in the array
+ *
+ * range: array rank
+ * 
+ * Return: The pointer to the newly created array
+ */
+
+int *array_range(int min, int max)
+{
+	int *new_array = NULL;
+	int range, z, c = 0;
+
+	if (min > max)
+		return (NULL);
+
+	range = max - min + 1;
+	new_array = malloc(range * sizeof(int));
+
+	if (!new_array)
+	{
+		free(new_array);
+		return (NULL);
+	}
+
+	for (z = min; z <= max; z++)
+	{
+		new_array[c] = z;
+		c++;
+	}
+
+	return (new_array);
+}
