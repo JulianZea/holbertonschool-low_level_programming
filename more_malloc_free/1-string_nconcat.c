b20c3e166addfc2158b82d1158bbd710df4e9176
root <root@DESKTOP-L44MSJ2.localdomain>
2022-06-28 23:44:45 -0500

    task 1-1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,71 +1,41 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * count_string - Get the size of a string.
- *
- * @string: base string.
- *
- * Return: pointer to a new space on memory, otherwise return NULL.
+ * *string_nconcat - malloa and If it fails exit with 98 value
+ * @s1: 1st string
+ * @s2: 2nd string
+ * @n: N numbers of the second string
+ * Return: s3.
  */
-
-unsigned int count_string(char *string)
-{
-	int i = 0;
-
-	while (string[i])
-		i++;
-	return (i);
-}
-
-/**
- * string_nconcat - concatenates two strings.
- *
- * @s1: base string.
- *
- * @s2: complementary string.
- *
- * @n: number of characters to take of s2.
- *
- * Return: pointer to a new space on memory, otherwise return NULL.
- */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, size_of_s1 = 0, size_of_s2 = 0, size_of_final_string = 0;
-	int holder = n;
-	char *final_string = NULL;
+	char *s3;
+	int z;
+	unsigned int c;
 
-	if (s1)
-		size_of_s1 = count_string(s1);
-	if (s2)
-		size_of_s2 = count_string(s2);
-
-	if (n >= size_of_s2)
-		n = size_of_s2;
-
-	if (holder >= 0)
-		size_of_final_string = size_of_s1 + n + 1;
-	else
-		size_of_final_string = size_of_s1 + 1;
-
-	if (s1 || s2)
-		final_string = malloc(size_of_s1 + n + 1 * sizeof(char));
-	else if (!s1 && !s2)
-		final_string = malloc(1);
-
-	if (!final_string)
-	{
-		free(final_string);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	z = 0;
+	while (s1[z] != '\0')
+		z++;
+	s3 = malloc(sizeof(char) * (z + n + 1) + 5);
+	if (s3 == NULL)
 		return (NULL);
-	}
-
-	for (i = 0; i < (size_of_final_string - 1); i++)
+	z = c = 0;
+	while (s1[z] != '\0')
 	{
-		if (i < size_of_s1)
-			final_string[i] = s1[i];
-		else if (i >= size_of_s1)
-			final_string[i] = s2[i - size_of_s1];
+		s3[z] = s1[z];
+		z++;
 	}
-	final_string[i] = '\0';
-	return (final_string);
+	while (c < n && s2[c] != '\0')
+	{
+		s3[z] = s2[c];
+		z++, c++;
+	}
+	s3[z] = '\0';
+	return (s3);
 }
