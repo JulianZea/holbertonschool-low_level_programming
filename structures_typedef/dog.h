88ebf36ae718fa4e858f7a7ee426d393122dc95c
root <root@DESKTOP-L44MSJ2.localdomain>
2022-06-30 13:20:57 -0500

    task 3-0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - New struct dog with name, age, owner
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of dog
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif _DOG_H_
