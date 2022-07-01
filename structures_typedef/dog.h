a1f48860ddd9d25d948acf5cfc6c50a51af86ce0
root <root@DESKTOP-L44MSJ2.localdomain>
2022-06-30 21:46:04 -0500

    task 3-0new

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,11 +1,16 @@
 #ifndef _DOG_H_
 #define _DOG_H_
+
 /**
- * struct dog - New struct dog with name, age, owner
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of dog
-*/
+ * struct dog - Basic atributtes of a dog
+ *
+ * @name: Name of the dog
+ *
+ * @age: Age of the dog
+ *
+ * @owner: Owner of the dog
+ */
+
 struct dog
 {
 	char *name;
@@ -13,4 +18,34 @@ struct dog
 	char *owner;
 };
 
-#endif _DOG_H_
+typedef struct dog dog_t#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ *  * struct dog - Basic atributtes of a dog
+ *   *
+ *    * @name: Name of the dog
+ *     *
+ *      * @age: Age of the dog
+ *       *
+ *        * @owner: Owner of the dog
+ *         */
+
+struct dog
+{
+		char *name;
+			float age;
+				char *owner;
+};
+
+typedef struct dog dog_t;
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /* DOG_H */
