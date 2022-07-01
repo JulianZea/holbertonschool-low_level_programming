f0f26d17ed3b4851154379b48daf07a2b3b516fe
root <root@DESKTOP-L44MSJ2.localdomain>
2022-06-30 21:48:12 -0500

    task 3-0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -18,26 +18,6 @@ struct dog
 	char *owner;
 };
 
-typedef struct dog dog_t#ifndef _DOG_H_
-#define _DOG_H_
-
-/**
- *  * struct dog - Basic atributtes of a dog
- *   *
- *    * @name: Name of the dog
- *     *
- *      * @age: Age of the dog
- *       *
- *        * @owner: Owner of the dog
- *         */
-
-struct dog
-{
-		char *name;
-			float age;
-				char *owner;
-};
-
 typedef struct dog dog_t;
 
 #include <stdlib.h>
