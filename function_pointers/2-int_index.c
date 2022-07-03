9422da03bc8bc9d8fb8d87b5f048293d5d8f0289
root <root@DESKTOP-L44MSJ2.localdomain>
2022-07-02 21:40:10 -0500

    task 4-2

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,31 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - Searches for an integer.
+ *
+ * @array: An array with numbers (int)
+ *
+ * @size: number of elements of the array
+ *
+ * @cmp: function for do the search
+ *
+ * Return: The index of the first element that match or -1 otherwise.
+ * if size is less than 0 return -1.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int z = 0, resultElement = 0;
+
+	if (size <= 0 || !array || !cmp)
+		return (-1);
+
+	for (z = 0; z < size; z++)
+	{
+		resultElement = cmp(array[i]);
+
+		if (resultElement != 0)
+			return (z);
+	}
+	return (-1);
+}
