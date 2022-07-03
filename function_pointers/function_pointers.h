bb464ca87315fa169d8b6a7f2b39100a33224ef6
root <root@DESKTOP-L44MSJ2.localdomain>
2022-07-02 21:47:44 -0500

    task 4-2actualizando

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -7,6 +7,7 @@ typedef unsigned long size_t;
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif /*C_FUNCTION_POINTERS*/
 
