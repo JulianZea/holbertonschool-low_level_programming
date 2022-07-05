7fb5e192c9c7bc7e73eba0fd6a7696e7250e18d3
root <root@DESKTOP-L44MSJ2.localdomain>
2022-07-05 02:52:50 -0500

    task 1-3 new

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -9,7 +9,6 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-#endif /*VARIADIC_FUNCTIONS_H*/
 
 /**
  * struct dt - struct
@@ -22,4 +21,4 @@ typedef struct dt
 	void (*func)(va_list);
 } datatype;
 
-#endif
+#endif /*VARIADIC_FUNCTIONS_H*/
