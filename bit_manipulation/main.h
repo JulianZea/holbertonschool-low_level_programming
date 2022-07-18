189af178136ab7be35fe0c565eba8a9938c8fd77
JulianZea <cesarjulianzeabravo@gmail.com>
2022-07-18 18:36:04 -0500

    header and prototype

---
+++ bit_manipulation/main.h
@@ -0,0 +1,13 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdlib.h> /* NULL */
+
+/* helper function */
+int _putchar(char c);
+
+/* function prototypes for all files */
+/*task 0*/
+unsigned int binary_to_uint(const char *b);
+
+#endif
