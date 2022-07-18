3852157eedc268adbb411464f39129af9b87d9fc
JulianZea <cesarjulianzeabravo@gmail.com>
2022-07-18 17:52:24 -0500

    arreglando

---
+++ bit_manipulation/manipulation.h
@@ -0,0 +1,12 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdlib.h> /* NULL */
+
+/* helper function */
+int _putchar(char c);
+
+/* function prototypes for all files */
+unsigned int binary_to_uint(const char *b);
+
+#endif
