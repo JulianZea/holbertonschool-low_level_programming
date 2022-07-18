4466c88c94d2796340e3f268507ded0bfe69cbd1
JulianZea <cesarjulianzeabravo@gmail.com>
2022-07-18 18:10:39 -0500

    prototype header

--- bit_manipulation/manipulation.h
+++ bit_manipulation/manipulation.h
@@ -7,6 +7,15 @@
 int _putchar(char c);
 
 /* function prototypes for all files */
+/*task 0*/
 unsigned int binary_to_uint(const char *b);
+/*task 1*/
+/*task 2*/
+/*task 3*/
+/*task 4*/
+/*task 5*/
+/*task 6*/
+/*task 7*/
+/*task 8*/
 
-#endif
+#endif /*HEADER_H*/
