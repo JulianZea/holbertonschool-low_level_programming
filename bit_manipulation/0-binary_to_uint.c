394feae80d0d365e51a4a374d0b287e89d95fe05
JulianZea <cesarjulianzeabravo@gmail.com>
2022-07-18 18:42:57 -0500

    convert a binary number to an integer

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,37 +1,35 @@
 #include "main.h"
 
 /**
- * binary_to_uint - convert binary to unsigned int
- * @b: binary
- * Return: unsigned int
+ * binary_to_uint - Converts a binary number to an unsigned int.
+ *
+ * @b: Pointer to a string with a binary number
+ *
+ * Return: The converted number, or 0 if:
+ * - There is one or more chars in the string b that is not 0 or 1
+ * - b is NULL
  */
+
 unsigned int binary_to_uint(const char *b)
 {
+	unsigned int converted_number = 0, position = 1;
+	int z = 0;
 
-	int len = 0, z;
-	unsigned int sum = 0;
+	if (!b)
+		return (0);
 
-	if (b == NULL)
-		return (sum);
-
-	/* find string length */
-	while (b[len] != '\0')
-		len++;
-	len -= 1;
-
-	/* iterate string and if '1' then multiply by power of 2 */
-	/* get power of 2 via binary (e.g. 1<<2 = 100 in binary = 4) */
-	z = 0;
 	while (b[z])
-	{
-		if ((b[z] != '0') && (b[z] != '1'))
-			return (sum);
-
-		if (b[z] == '1')
-			sum += (1 * (1 << len));
 		z++;
-		len--;
+
+	z--;
+
+	for (; z >= 0; z--, position *= 2)
+	{
+		if (b[z] < '0' || b[z] > '1')
+			return (0);
+
+		converted_number += (position * (b[z] - '0'));
 	}
 
-	return (sum);
+	return (converted_number);
 }
