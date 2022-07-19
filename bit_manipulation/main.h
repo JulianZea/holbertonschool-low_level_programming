de53b0dd5fc45a8b345bea97aab7716f776c1003
JulianZea <cesarjulianzeabravo@gmail.com>
2022-07-19 00:14:28 -0500

    adding binary prototype

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -9,5 +9,12 @@ int _putchar(char c);
 /* function prototypes for all files */
 /*task 0*/
 unsigned int binary_to_uint(const char *b);
+/*task 1*/
+void print_binary(unsigned long int n);
+/*task 2*/
+/*task 3*/
+/*task 4*/
+/*task 5*/
+/*task 6*/
 
 #endif
