c5ea809666f064bf6e08421b871b6c7684e55f06
JulianZea <cesarjulianzeabravo@gmail.com>
2022-07-19 00:25:55 -0500

    that returns the value of a bit at a given index

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -12,6 +12,7 @@ unsigned int binary_to_uint(const char *b);
 /*task 1*/
 void print_binary(unsigned long int n);
 /*task 2*/
+int get_bit(unsigned long int n, unsigned int index);
 /*task 3*/
 /*task 4*/
 /*task 5*/
