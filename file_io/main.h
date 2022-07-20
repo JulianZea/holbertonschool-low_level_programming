950c277d1a78238ea0b3a4bc8b0e325950eace5f
JulianZea <cesarjulianzeabravo@gmail.com>
2022-07-20 13:51:24 -0500

    adding prototype

---
+++ file_io/main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* open */
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/* close */
+#include <unistd.h>
+
+/* dprintf */
+#include <stdio.h>
+
+/* malloc, free */
+#include <stdlib.h>
+
+/*function printf*/
+int _putchar(char c);
+/*task 0*/
+ssize_t read_textfile(const char *filename, size_t letters);
+/*task 1*/
+/*task 2*/
+/*task 3*/
+
+#endif
