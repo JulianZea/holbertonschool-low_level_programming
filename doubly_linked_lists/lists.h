b2bbb762d1bd2a408b2356b4b4fe42c2210b8c4a
JulianZea <cesarjulianzeabravo@gmail.com>
2022-07-21 14:24:01 -0500

    struc doubly linked list, prototype task 0

---
+++ doubly_linked_lists/lists.h
@@ -0,0 +1,38 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h> /* printf */
+#include <stdlib.h> /* malloc, free */
+
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ * for Holberton project
+ */
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
+
+/* function prototypes */
+
+/* task 0 */
+size_t print_dlistint(const dlistint_t *h);
+/* task 1 */
+
+/* task 2 */
+
+/* task 3 */
+
+/* task 4 */
+
+/* task 5 */
+
+
+#endif
