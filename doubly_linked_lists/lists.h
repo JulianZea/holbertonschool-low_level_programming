302e1e0cad079348ba2b472494e316b3d544add4
JulianZea <cesarjulianzeabravo@gmail.com>
2022-07-24 13:54:03 -0500

    adding task 5 prototype

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -33,6 +33,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 /* task 4 */
 void free_dlistint(dlistint_t *head);
 /* task 5 */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 
 
 #endif
